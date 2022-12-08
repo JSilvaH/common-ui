6b34a373b4a212c11da665fe208f513bfae62c75
JSilvaH <jjesussh31@gmail.com>
2022-12-08 17:14:05 -0600

    test MyCommonButton

--- common-ui/src/main/java/com/silvacomp/common_ui/MyCommonButton.kt
+++ common-ui/src/main/java/com/silvacomp/common_ui/MyCommonButton.kt
@@ -1,15 +1,15 @@
 package com.silvacomp.common_ui
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun MyButton(text: String) {
+fun MyCommonButton(
+    text: String
+) {
     Button(onClick = {
         Log.d("Test library", "PEPE")
     }) {
@@ -21,5 +21,5 @@ fun MyButton(text: String) {
 @Preview
 @Composable
 fun MyButtonPrev() {
-    MyButton("Test")
+    MyCommonButton("Test")
 }
